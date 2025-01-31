616872d382ce4356b58ff0bc97c877c52a722935
Fabian Bärenbold <fabian.baerenbold@eawag.ch>
2025-01-31 08:37:59 +0100

    Remove slash

--- scripts/main.py
+++ scripts/main.py
@@ -37,7 +37,7 @@ for file in files:
         gradients = False
         interpolate = False
         sensor = TemperatureChainV0(log=log)
-    elif "/v1/" in file:
+    elif "v1" in file:
         continue
         version = "v1"
         gradients = False
